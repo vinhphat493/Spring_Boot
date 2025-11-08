1c6651901a4e1d322868a0cebf70f55b32dbf7f2
Ferb <vinhphat493@gmail.com>
2025-11-08 19:02:19 +0700

    [Phat]: init course management project

---
+++ coursecurd/src/test/java/com/example/coursecurd/CoursecurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursecurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursecurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
